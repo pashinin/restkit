c36333df9c54a8006cb89353074c6db01bddb8b6
Sergey Pashinin <sergey@pashinin.com>
2014-08-18 16:23:15 +0400

    print fix

--- tests/009-test-oauth_filter.py
+++ tests/009-test-oauth_filter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -
 #
-# This file is part of restkit released under the MIT license. 
+# This file is part of restkit released under the MIT license.
 # See the NOTICE for more information.
 
 
@@ -11,7 +11,7 @@
 # Three-legged: http://oauth-sandbox.sevengoslings.net/three_legged
 # Key: bd37aed57e15df53
 # Secret: 0e9e6413a9ef49510a4f68ed02cd
-
+from __future__ import print_function
 try:
     from urlparse import parse_qs, parse_qsl
 except ImportError:
@@ -31,11 +31,11 @@ class oauth_request(object):
         'two_legged': '/two_legged',
         'three_legged': '/three_legged'
     }
-    
+
     consumer_key = 'bd37aed57e15df53'
     consumer_secret = '0e9e6413a9ef49510a4f68ed02cd'
     host = 'http://oauth-sandbox.sevengoslings.net'
-    
+
     def __init__(self, utype):
         self.consumer = Consumer(key=self.consumer_key,
                             secret=self.consumer_secret)
@@ -46,19 +46,19 @@ class oauth_request(object):
             'blah': 599999
         }
         self.url = "%s%s" % (self.host, self.oauth_uris[utype])
-        
+
     def __call__(self, func):
         def run():
             o = OAuthFilter('*', self.consumer)
             func(o, self.url, urllib.urlencode(self.body))
         run.func_name = func.func_name
         return run
-        
+
 @oauth_request('request_token')
 def test_001(o, u, b):
     r = request(u, filters=[o])
     t.eq(r.status_int, 200)
-    
+
 @oauth_request('request_token')
 def test_002(o, u, b):
     r = request(u, "POST", filters=[o])
@@ -66,14 +66,14 @@ def test_002(o, u, b):
     f = dict(parse_qsl(r.body_string()))
     t.isin('oauth_token', f)
     t.isin('oauth_token_secret', f)
-    
+
 
 @oauth_request('two_legged')
 def test_003(o, u, b):
     r = request(u, "POST", body=b, filters=[o],
                 headers={"Content-type": "application/x-www-form-urlencoded"})
     import sys
-    print >>sys.stderr, r.body_string()
+    print(r.body_string(), file=sys.stderr)
     t.eq(r.status_int, 200)
     # Because this is a POST and an application/x-www-form-urlencoded, the OAuth
     # can include the OAuth parameters directly into the body of the form, however
@@ -84,7 +84,3 @@ def test_003(o, u, b):
 def test_004(o, u, b):
     r = request(u, "GET", filters=[o])
     t.eq(r.status_int, 200)
-    
-    
-
-
