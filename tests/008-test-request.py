50241e8032bf3066a1401557b634bbd8bd62c7bc
Sergey Pashinin <sergey@pashinin.com>
2014-08-18 16:36:40 +0400

    style fixes

--- tests/008-test-request.py
+++ tests/008-test-request.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -
 #
-# This file is part of restkit released under the MIT license. 
+# This file is part of restkit released under the MIT license.
 # See the NOTICE for more information.
 
 import os
@@ -67,12 +67,14 @@ This is a relatively long body, that we send to the client...
 This is a relatively long body, that we send to the client...
 This is a relatively long body, that we send to the client..."""
 
+
 def test_001():
     u = "http://%s:%s" % (HOST, PORT)
     r = request(u)
     t.eq(r.status_int, 200)
     t.eq(r.body_string(), "welcome")
-    
+
+
 def test_002():
     u = "http://%s:%s" % (HOST, PORT)
     r = request(u, 'POST', body=LONG_BODY_PART)
@@ -80,51 +82,56 @@ def test_002():
     body = r.body_string()
     t.eq(len(body), len(LONG_BODY_PART))
     t.eq(body, LONG_BODY_PART)
-    
+
+
 def test_003():
-     u = "http://test:test@%s:%s/auth" % (HOST, PORT)
-     r = request(u)
-     t.eq(r.status_int, 200)
-     u = "http://test:test2@%s:%s/auth" % (HOST, PORT)
-     r = request(u)
-     t.eq(r.status_int, 403)
-     
+    u = "http://test:test@%s:%s/auth" % (HOST, PORT)
+    r = request(u)
+    t.eq(r.status_int, 200)
+    u = "http://test:test2@%s:%s/auth" % (HOST, PORT)
+    r = request(u)
+    t.eq(r.status_int, 403)
+
+
 def test_004():
     u = "http://%s:%s/multipart2" % (HOST, PORT)
     fn = os.path.join(os.path.dirname(__file__), "1M")
     f = open(fn, 'rb')
     l = int(os.fstat(f.fileno())[6])
-    b = {'a':'aa','b':['bb','éàù@'], 'f':f}
-    h = {'content-type':"multipart/form-data"}
+    b = {'a': 'aa', 'b': ['bb', 'éàù@'], 'f': f}
+    h = {'content-type': "multipart/form-data"}
     body, headers = multipart_form_encode(b, h, uuid.uuid4().hex)
     r = request(u, method='POST', body=body, headers=headers)
     t.eq(r.status_int, 200)
     t.eq(int(r.body_string()), l)
-    
+
+
 def test_005():
     u = "http://%s:%s/multipart3" % (HOST, PORT)
     fn = os.path.join(os.path.dirname(__file__), "1M")
     f = open(fn, 'rb')
     l = int(os.fstat(f.fileno())[6])
-    b = {'a':'aa','b':'éàù@', 'f':f}
-    h = {'content-type':"multipart/form-data"}
+    b = {'a': 'aa', 'b': 'éàù@', 'f': f}
+    h = {'content-type': "multipart/form-data"}
     body, headers = multipart_form_encode(b, h, uuid.uuid4().hex)
     r = request(u, method='POST', body=body, headers=headers)
     t.eq(r.status_int, 200)
     t.eq(int(r.body_string()), l)
-    
+
+
 def test_006():
     u = "http://%s:%s/multipart4" % (HOST, PORT)
     fn = os.path.join(os.path.dirname(__file__), "1M")
     f = open(fn, 'rb')
     content = f.read()
     f.seek(0)
-    b = {'a':'aa','b':'éàù@', 'f':f}
-    h = {'content-type':"multipart/form-data"}
+    b = {'a': 'aa', 'b': 'éàù@', 'f': f}
+    h = {'content-type': "multipart/form-data"}
     body, headers = multipart_form_encode(b, h, uuid.uuid4().hex)
     r = request(u, method='POST', body=body, headers=headers)
     t.eq(r.status_int, 200)
     t.eq(r.body_string(), content)
+
 
 def test_007():
     import StringIO
@@ -132,8 +139,8 @@ def test_007():
     content = 'éàù@'
     f = StringIO.StringIO('éàù@')
     f.name = 'test.txt'
-    b = {'a':'aa','b':'éàù@', 'f':f}
-    h = {'content-type':"multipart/form-data"}
+    b = {'a': 'aa', 'b': 'éàù@', 'f': f}
+    h = {'content-type': "multipart/form-data"}
     body, headers = multipart_form_encode(b, h, uuid.uuid4().hex)
     r = request(u, method='POST', body=body, headers=headers)
     t.eq(r.status_int, 200)
